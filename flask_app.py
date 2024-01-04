62cb59e8729096ec30e6352c2670506768669c8c
dhanunr252 <dhanunr8672@gmail.com>
2024-01-04 07:45:41 +0530

    flask initial commit

---
+++ flask_app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def defaultPage():
+    return "Hello Guest"
+
+@app.route("/app1")
+def app1Page():
+    return "Hello Guest - From App1"
+@app.route("/home/<string:name>")
+def GuestName(name):
+    return {"Hello": name}
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5002)
